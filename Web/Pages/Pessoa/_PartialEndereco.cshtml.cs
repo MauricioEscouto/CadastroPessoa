c0b76010514f4db838050642053d030f290c5ed6
Mauricio Escouto <mauriciocunha953@gmail.com>
2024-01-19 20:42:21 -0300

    [FIX] Ajustes de layout para ficar igual ao mockup

--- Web/Pages/Pessoa/_PartialEndereco.cshtml.cs
+++ Web/Pages/Pessoa/_PartialEndereco.cshtml.cs
@@ -1,13 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Web.Shared.Utilities;
 
 namespace Web.Pages.Pessoa
 {
     public class _PartialEnderecoModel : PageModel
     {
         public int Id { get; set; }
+        [BindProperty]
+        public List<string> Estados { get; set; }
 
         public void OnGet(int id)
         {
+            Estados = EstadosBrasil.ObterEstadosAbreviados();
             Id = id;
         }
     }
---
+++ Web/Shared/Utilities/EstadosBrasil.cs
@@ -0,0 +1,15 @@
+﻿namespace Web.Shared.Utilities
+{
+	public static class EstadosBrasil
+	{
+		public static List<string> ObterEstadosAbreviados()
+		{
+			return new List<string>
+			{
+				"AC", "AL", "AP", "AM", "BA", "CE", "DF", "ES", "GO", "MA",
+				"MT", "MS", "MG", "PA", "PB", "PR", "PE", "PI", "RJ", "RN",
+				"RS", "RO", "RR", "SC", "SP", "SE", "TO"
+			};
+		}
+	}
+}
